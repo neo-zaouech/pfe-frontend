ddd50a09e3c559859cb0ddf9013cbe92c3a57449
neo-zaouech <nadhirfrp@gmail.com>
2023-04-05 01:40:35 +0100

    First commit branch about

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -1,8 +1,8 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
 
 const About = () => {
-  return <Box></Box>
-}
+  return <Box>Hello</Box>;
+};
 
-export default About
+export default About;
